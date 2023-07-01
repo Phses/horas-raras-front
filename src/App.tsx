3691819b2d44456689428e7a5599e74ab740bc39
unknown <pedro.silva@rarolabs.com.br>
2023-07-01 11:04:51 -0300

    fix: test pre-commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
     <div>Teste</div>
+    <div>Agora vai</div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
