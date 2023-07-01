ba357c593a678f549e62a20aad8fcb534462f4de
unknown <pedro.silva@rarolabs.com.br>
2023-07-01 16:56:37 -0300

    test: testes remote authentication

---
+++ src/domain/test/mock-auth-params.ts
@@ -0,0 +1,9 @@
+import { AuthParams } from "../models"
+import { faker } from "@faker-js/faker"
+
+export const mockAuthParams = (): AuthParams => {
+    return {
+        email: faker.internet.email(),
+        password: faker.internet.password()
+    }
+}
