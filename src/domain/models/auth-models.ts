e89ec7034f2e2e6577e14f513f254d14734c2e7e
unknown <pedro.silva@rarolabs.com.br>
2023-07-01 15:53:37 -0300

    feat: adicionando abstracao do authentication

---
+++ src/domain/models/auth-models.ts
@@ -0,0 +1,8 @@
+export type AuthParams = {
+    email: string
+    password: string
+}
+
+export type AuthToken = {
+    token: string
+}
---
+++ src/domain/usecases/authentication/authentication.ts
@@ -0,0 +1,5 @@
+import { AuthParams, AuthToken } from '@/domain/models';
+
+export interface Authentication {
+    auth(params: AuthParams): Promise<AuthToken> 
+}
